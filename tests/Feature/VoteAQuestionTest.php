fdfd42b276d50d05ca6b1d33a2510c9c9f51006c
Rafael Pozzebon <rafael@unitronica.com.br>
2023-10-30 14:35:38 -0300

    HEY-4: onclik like and question only once

--- tests/Feature/VoteAQuestionTest.php
+++ tests/Feature/VoteAQuestionTest.php
@@ -2,7 +2,7 @@
 
 use App\Models\{Question, User};
 
-use function Pest\Laravel\{actingAs, assertDatabaseHas};
+use function Pest\Laravel\{actingAs, assertDatabaseHas, post};
 
 it('should be able to like a question', function () {
     // Arrange
@@ -25,3 +25,24 @@ it('should be able to like a question', function () {
 });
 
 todo('should be able to unlike a question');
+
+it('should be able to like a question only once', function () {
+    $user     = User::factory()->create();
+    $question = Question::factory()->create();
+
+    actingAs($user);
+
+    post(route('question.vote', $question->id));
+    post(route('question.vote', $question->id));
+    post(route('question.vote', $question->id));
+    post(route('question.vote', $question->id));
+
+    assertDatabaseHas('votes', [
+        'question_id' => $question->id,
+        'user_id'     => $user->id,
+        'like'        => 1,
+        'unlike'      => 0,
+    ]);
+
+    expect($user->votes()->count())->toBe(1);
+});
