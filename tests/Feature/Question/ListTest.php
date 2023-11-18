4247dd0ce6ff217e483e64b283ee8b2e8c64082c
Rafael Pozzebon <rafael@unitronica.com.br>
2023-11-18 18:20:02 -0300

    wip

--- tests/Feature/Question/ListTest.php
+++ tests/Feature/Question/ListTest.php
@@ -37,14 +37,35 @@ it('should list all questions', function () {
 });
 
 it('should paginate the result', function () {
-    // Arrange
-    $questions = Question::factory()->count(20)->create();
+    Question::factory()->count(20)->create();
 
     $user = User::factory()->create();
     actingAs($user);
 
-    //Act
-    $response = get(route('dashboard'))
-        ->assertViewHas('questions', fn ($value) => $value instanceof \Illuminate\Pagination\LengthAwarePaginator);
+    get(route('dashboard'))
+        ->assertViewHas('questions', fn($value) => $value instanceof \Illuminate\Pagination\LengthAwarePaginator);
+});
 
+it('should order by like and unlike, most liked question should be at the top, most unliked questions should be in the bottom', function () {
+    $user = User::factory()->create();
+    $secondUser = User::factory()->create();
+    Question::factory()->count(5)->create();
+
+    $mostLikedQuestion = Question::find(3);
+    $user->like($mostLikedQuestion);
+
+    $mostUnlikedQuestion = Question::find(1);
+    $secondUser->unlike($mostUnlikedQuestion);
+
+    actingAs($user);
+    get(route('dashboard'))
+        ->assertViewHas('questions', function ($questions) {
+
+            expect($questions)
+                ->first()->id->toBe(3)
+                ->and($questions)
+                ->last()->id->toBe(1);
+
+            return true;
+        });
 });
