847ff4ca55c05149fd76f26c5578b719bf04c640
rafael_pozzebon <rafael@unitronica.com.br>
2023-11-20 08:35:21 -0300

    develop

--- app/Http/Controllers/Auth/Github/RedirectController.php
+++ app/Http/Controllers/Auth/Github/RedirectController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Auth\Github;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
 
 class RedirectController extends Controller
